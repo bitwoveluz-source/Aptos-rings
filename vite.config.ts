ba96c2f01213a9f2ffb420fbde5e2666578d9d5e
PEKER <adrianneveluz@gmail.com>
2025-10-19 19:33:22 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@telegram-apps/bridge']
+
   },
   server: {
     port: 3000,
